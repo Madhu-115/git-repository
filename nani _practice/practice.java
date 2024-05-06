f8bc6168932f1ccb07b482ee06133d6447c129b1
Madhu-115 <166009938+Madhu-115@users.noreply.github.com>
2024-05-06 12:14:43 +0530

    Add files via upload

---
+++ nani _practice/practice.java
@@ -0,0 +1,17 @@
+//hello nani how e u?
+//1234567
+//a=1.2
+//hello + hi =hello hi
+
+class practice  
+{	
+	public static void main (String[]arg)
+	{	
+		System.out.println(1234567);
+
+	}
+
+}
+
+	
+	//System.out.println("hello nani how e u?");
